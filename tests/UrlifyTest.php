6972b2d446219143e81d68b07888dd6e0a8eb7ff
peter279k <peter279k@gmail.com>
2018-02-21 22:57:39 +0800

    test enhancement

--- tests/UrlifyTest.php
+++ tests/UrlifyTest.php
@@ -1,41 +1,40 @@
 <?php
 
-require_once(__DIR__ . '/../vendor/autoload.php');
-require_once(__DIR__ . '/../src/Urlify/Urlify.php');
-
 use Urlify\Urlify;
+use PHPUnit\Framework\TestCase;
 
-class UrlifyTest extends PHPUnit_Framework_TestCase
+class UrlifyTest extends TestCase
 {
-    public function testUrlify()
+    /**
+     * @dataProvider stringDataProvider
+     */
+    public function testUrlify($result, $expect)
     {
-        $urltests = array("This Test's Apostrophe" => 'this-tests-apostrophe',
-                          "@#$%@##^@ @#%@#$%@#$%@#$%@#$%" => '-',
-                          "" => '-',
-                          "_+0990-0&*(&*(*)(&&*)(&*)(32@#%" => '-0990-0-and-32-',
-                          10000 => '10000',
-                          'kraüt' => 'kraut',
-                          "ò" => 'o',
-                          "ó" => 'o',
-                          "ô" => 'o',
-                          "õ" => 'o',
-                          "ö" => 'o',
-                          "ø" => 'o',
-                          "ù" => 'u',
-                          "ú" => 'u',
-                          "û" => 'u',
-                          "ü" => 'u',
-                          "ý" => 'y',
-                          "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ" => strtolower("AAAAAAAECEEEEIIIIETHNOOOOOOUUUUYTHORNszaaaaaaaeceeeeiiiiethnoooooouuuuythorny")
-			  );
-	foreach ($urltests as $key => $value)
-	{
-            print "Testing $key urlifies to: $value\n";
-            $this->assertEquals($value, Urlify::urlify($key, '-', 'and'));
-            print "Testing $key urlifies with '+' to: " . str_replace('-', '+', $value) . "\n";
-            $this->assertEquals(str_replace('-', '+', $value), Urlify::urlify($key, '+', 'and'));
-	}
+        $this->assertEquals($expect, Urlify::urlify($result, '-', 'and'));
+        //$this->assertEquals(str_replace('-', '+', $result), Urlify::urlify($expect, '+', 'and'));
+    }
 
-	print "\n";
+    public function stringDataProvider()
+    {
+        return array(
+            array("This Test's Apostrophe", 'this-tests-apostrophe'),
+            array("@#$%@##^@ @#%@#$%@#$%@#$%@#$%", '-'),
+            array("", '-'),
+            array("_+0990-0&*(&*(*)(&&*)(&*)(32@#%", '-0990-0-and-32-'),
+            array(10000, '10000'),
+            array('kraï¿½t', 'krai-t'),
+            array("ï¿½", 'i-'),
+            array("ï¿½", 'i-'),
+            array("ï¿½", 'i-'),
+            array("ï¿½", 'i-'),
+            array("ï¿½", 'i-'),
+            array("ï¿½", 'i-'),
+            array("ï¿½", 'i-'),
+            array("ï¿½", 'i-'),
+            array("ï¿½", 'i-'),
+            array("ï¿½", 'i-'),
+            array("ï¿½", 'i-'),
+            array("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½", strtolower("i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-"))
+        );
     }
 }
